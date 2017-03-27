6b919ac78c1c9af464628bbba7f40b867c1f0de7
wonseok <ricemen@outlook.kr>
2017-03-27 23:48:30 +0900

    160 page

--- src/main/java/springbook/learningtest/spring/scope/ScopeTest.java
+++ src/main/java/springbook/learningtest/spring/scope/ScopeTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 public class ScopeTest {
 
@@ -22,7 +23,7 @@ public class ScopeTest {
 		beans.add(ac.getBean(SingletonBean.class));
 		
 		assertThat(beans.size(), is(1));
-		 
+		
 		beans.add(ac.getBean(SingletonClientBean.class).bean1);
 		beans.add(ac.getBean(SingletonClientBean.class).bean2);
 		
@@ -33,6 +34,32 @@ public class ScopeTest {
 	static class SingletonClientBean {
 		@Autowired
 		SingletonBean bean1;
+		@Autowired
 		SingletonBean bean2;		
+	}
+	
+	@Test
+	public void prototypeScope() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class, PrototypeClientBean.class);
+		Set<PrototypeBean> beans = new HashSet<>();
+		
+		beans.add(ac.getBean(PrototypeBean.class));
+		assertThat(beans.size(), is(1));
+		beans.add(ac.getBean(PrototypeBean.class));
+		assertThat(beans.size(), is(2));
+		
+		beans.add(ac.getBean(PrototypeClientBean.class).bean1);
+		assertThat(beans.size(), is(3));
+		beans.add(ac.getBean(PrototypeClientBean.class).bean2);
+		assertThat(beans.size(), is(4));
+	}
+	
+	@Scope("prototype")
+	static class PrototypeBean {}
+	static class PrototypeClientBean {
+		@Autowired
+		PrototypeBean bean1;
+		@Autowired
+		PrototypeBean bean2;		
 	}
 }
